2441f2b490cbe88700930681fbe5e7dd26946efa
Adrian Chang <adrian_chang@brown.edu>
2022-09-12 20:53:12 -0400

    comment standardize

--- src/glrenderer.cpp
+++ src/glrenderer.cpp
@@ -11,7 +11,7 @@ GLRenderer::GLRenderer(QWidget *parent)
 
 void GLRenderer::finish()
 {
-    //TASK 19 Clean up you VBO and VAO memory here
+    // Task 19: Clean up you VBO and VAO memory here
 
     glDeleteProgram(m_shader);
     doneCurrent();
@@ -24,47 +24,47 @@ void GLRenderer::initializeGL()
     if (GLEW_OK != err) fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
     fprintf(stdout, "Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
-    // TASK 4: Set the clear color here
+    // Task 4: Set the clear color here
 
     m_shader = ShaderLoader::createShaderProgram("Resources/Shaders/default.vert", "Resources/Shaders/default.frag"); //Shader setup (DO NOT EDIT)
 
 
     // Vertex Buffer Objects //
 
-    // TASK 5: Generate a VBO here and store it in m_vbo
+    // Task 5: Generate a VBO here and store it in m_vbo
 
-    // TASK 6: Bind the VBO you created here
+    // Task 6: Bind the VBO you created here
 
-    // TASK 8: Construct your std::vector of triangle data here | TASK 10: Add colors to your triangle here
+    // Task 8: Construct your std::vector of triangle data here | TASK 10: Add colors to your triangle here
 
-    // TASK 9: Pass the triangle vector into your VBO here
+    // Task 9: Pass the triangle vector into your VBO here
 
 
     // Vertex Array Objects //
 
-    // TASK 11: Generate a VAO here and store it in m_vao
+    // Task 11: Generate a VAO here and store it in m_vao
 
-    // TASK 12: Bind the VAO you created here
+    // Task 12: Bind the VAO you created here
 
-    // TASK 13: Add position and color attributes to your VAO here
+    // Task 13: Add position and color attributes to your VAO here
 
 
     // Returning to Default State //
 
-    //TASK 14: Unbind your VBO and VAO here
+    // Task 14: Unbind your VBO and VAO here
 }
 
 void GLRenderer::paintGL()
 {
-    //TASK 15: Clear the screen here
+    // Task 15: Clear the screen here
 
     glUseProgram(m_shader);
 
-    //TASK 16: Bind your VAO here
+    // Task 16: Bind your VAO here
 
-    //TASK 17: Draw your VAO here
+    // Task 17: Draw your VAO here
 
-    //TASK 18: Unbind your VAO here
+    // Task 18: Unbind your VAO here
 
     glUseProgram(0);
 }
